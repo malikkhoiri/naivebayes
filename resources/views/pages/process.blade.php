5917bfed3dbdba45c822776d5f93f55d6c4df7a4
Malik <abdlchoiri19@gmail.com>
2019-07-22 10:55:49 +0700

    make feture process to simulate naive bayes

---
+++ resources/views/pages/process.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="main">
+        <div class="main-content">
+            <div class="container-fluid">
+                <div class="panel panel-headline">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Testing</h3>
+                    </div>
+                    <div class="panel-body">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
