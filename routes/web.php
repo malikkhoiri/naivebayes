0b51c98d411a593234f5dec3528aa4b904928d04
Malik <abdlchoiri19@gmail.com>
2019-08-08 19:31:08 +0700

    update routes and add middleware group

--- routes/web.php
+++ routes/web.php
@@ -12,30 +12,34 @@
 */
 
 Route::get('/', function () {
-    return view('layouts.app');
+    return redirect()->route('login');
 });
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/question', 'QuestionController@index');
-Route::post('/question/upload', 'QuestionController@upload')->name('uploadQuestion');
-Route::get('/question/download/{id}', 'QuestionController@download')->name('downloadQuestion');
-Route::get('/question/delete/{id}', 'QuestionController@destroy')->name('deleteQuestion');
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('/question', 'QuestionController@index');
+    Route::post('/question/upload', 'QuestionController@upload')->name('uploadQuestion');
+    Route::get('/question/download/{id}', 'QuestionController@download')->name('downloadQuestion');
+    Route::get('/question/delete/{id}', 'QuestionController@destroy')->name('deleteQuestion');
 
-Route::get('/answer', 'AnswerController@index');
-Route::post('/answer/upload', 'AnswerController@upload')->name('uploadAnswer');
-Route::get('/answer/download/{id}', 'AnswerController@download')->name('downloadAnswer');
-Route::get('/answer/delete/{id}', 'AnswerController@destroy')->name('deleteAnswer');
+    Route::get('/answer', 'AnswerController@index');
+    Route::post('/answer/upload', 'AnswerController@upload')->name('uploadAnswer');
+    Route::get('/answer/download/{id}', 'AnswerController@download')->name('downloadAnswer');
+    Route::get('/answer/delete/{id}', 'AnswerController@destroy')->name('deleteAnswer');
 
-Route::get('/analysis', 'AnalysisController@index');
-Route::get('/analysis/process', 'AnalysisController@analysis')->name('analysis');
-Route::get('/analysis/export-pdf', 'AnalysisController@analysis')->name('exportPdf');
-Route::get('/analysis/tes', 'AnalysisController@analysis');
+    Route::get('/analysis', 'AnalysisController@index');
+    Route::get('/analysis/process', 'AnalysisController@analysis')->name('analysis');
+    Route::get('/analysis/export-pdf', 'AnalysisController@analysis')->name('exportPdf');
+    Route::get('/analysis/tes', 'AnalysisController@analysis');
 
-Route::get('/process', 'ProcessController@index');
-Route::get('/process/testing', 'ProcessController@testing');
+    Route::get('/process', 'ProcessController@index');
+    Route::get('/process/testing', 'ProcessController@testing');
 
-Route::get('/training', 'DataTrainingController@index');
-Route::post('/training/import-csv', 'DataTrainingController@import_csv')->name('importCSV');
+    Route::get('/training', 'DataTrainingController@index');
+    Route::post('/training/import-csv', 'DataTrainingController@import_csv')->name('importCSV');
+
+    Route::resource('user', 'UserController');
+});
